d02df2ccd461614068afe53b8660b5dd904938fb
Vladimir Pavlov <pavloff.com@gmail.com>
2022-05-16 21:00:17 +0500

    Create HTTP Server with Express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!")
+
+import express, {Express, Request, Response} from "express";
+import "dotenv/config";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Web Server is already running")
+});
+
+
+
