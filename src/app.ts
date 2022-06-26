2756abac6bf086a27c827ffc88e7458846003818
Vladimir Pavlov <pavloff.com@gmail.com>
2022-06-27 02:03:41 +0500

    Add web server listener

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-
 import express, {Express, Request, Response} from "express";
 import "dotenv/config";
 
 const app: Express = express();
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Web Server is already running")
 });
 
-
+app.listen(port, () => {
+    console.log(`Web Server listening on port ${port}`)
+})
 
